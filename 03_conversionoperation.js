eca7fcfc8408a0d51d31dfe4d684427cc04b2f15
Anuragkr001 <mranuragkumar91@gmail.com>
2025-08-31 02:27:27 +0530

    conversion

--- 03_conversionoperation.js
+++ 03_conversionoperation.js
@@ -1,19 +1,43 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueinNumber= Number(score)
-console.log(typeof valueinNumber);
-console.log(valueinNumber);
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
 // "33" --> 33
 // "33abc" --> NaN
 // true -->1; false -> 0
 let islogedin = "hitesh"
 
 let booleanIsLoggedIn = Boolean(islogedin)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
  
 // 1--> true; 0-->false
 //  ""--> false
 // "hitesh" --> true
+
+
+// ********************operation ****************
+
+let value=3
+let negValue= -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let s1="anurag"
+let s2=" kumar"
+let s3=s1+s2
+console.log(s3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
