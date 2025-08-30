8d4210ddf14856c4a043be2f01005b85427b42d1
Anuragkr001 <mranuragkumar91@gmail.com>
2025-08-31 01:45:16 +0530

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,12 @@
+let name="anurag"
+let age=22
+let isadult=true
+// number=> 2 to power 53
+// bigint
+// string==> ""
+//null==>stadealone value
+//undefined
+// boolean==>true/false
+//symbol==> unique
+console.log(typeof"anurag");
+console.log(typeof null);
