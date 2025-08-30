2ed1115707410f0112a17a7c6dbda297b34a967f
Anuragkr001 <mranuragkumar91@gmail.com>
2025-08-31 02:09:03 +0530

    js

---
+++ 03_conversionoperation.js
@@ -0,0 +1,19 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinNumber= Number(score)
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+// "33" --> 33
+// "33abc" --> NaN
+// true -->1; false -> 0
+let islogedin = "hitesh"
+
+let booleanIsLoggedIn = Boolean(islogedin)
+console.log(booleanIsLoggedIn);
+ 
+// 1--> true; 0-->false
+//  ""--> false
+// "hitesh" --> true
