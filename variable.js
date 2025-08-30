c0fe44621a432515fbba9f510cb7ec7db5b8bb2d
Anuragkr001 <mranuragkumar91@gmail.com>
2025-08-31 01:26:10 +0530

    1st_day

--- variable.js
+++ variable.js
@@ -1,5 +1,8 @@
-// const accountId=144553
-// let accountEmail = "anurag@google.com"
-// var accountPassword = "12345"
-// accountcity = "jaipur"
-console.log("accountId")
+const accountId=144553
+let accountEmail = "anurag@google.com"
+var accountPassword = "12345"
+accountcity = "jaipur"
+let accountstate;
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountcity, accountstate])
+
