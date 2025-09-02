2d3bcb2c17fc01dda15b112ed9fd2fd4ab4f9b6b
Anuragkr001 <mranuragkumar91@gmail.com>
2025-09-02 12:11:13 +0530

    js

---
+++ 05_string.js
@@ -0,0 +1,36 @@
+const name = "Anurag"
+const repocnt=50
+// console.log(name + repocnt + " value");
+console.log(`Hello My name is ${name} and my repo cnt is ${repocnt}`);
+
+const gameName= new String('anurag-kr-01')
+
+// console.log(gameName[3]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'));
+
+console.log(gameName.substring(0,4));
+console.log(gameName.slice(-7,4));
+
+const newString = "  anurag  "
+console.log(newString);
+console.log(newString.trim());
+
+const url = "https://Anurag.com/Anurag%20Kumar"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('harry'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
---
+++ 07_datesinjs.js
@@ -0,0 +1,10 @@
+// DATES
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+ let myCreateddata = new Date(2025, 9, 15, 5, 3)
+ console.log(myCreateddata.toLocaleString());
+
+
